9241e46b07f1087d4b094400b57e9130c37c9cdc
Stusigh <stuartjohnpage@live.com>
2021-01-27 12:12:15 -0600

    initial commit

---
+++ src/AsciiChars.java
@@ -0,0 +1,34 @@
+public class AsciiChars {
+    public static void printNumbers()
+    {
+        // TODO: print valid numeric input
+        System.out.println("You've reached the printNumbers method.");
+        for(int i = 48; i<=57; ++i) {
+            int asciiValue = i;
+            char validCharacter = (char)asciiValue;
+            System.out.println(validCharacter);
+        }
+    }
+
+    public static void printLowerCase()
+    {
+        // TODO: print valid numeric input
+        System.out.println("You've reached the printLowerCase method.");
+        for(int i = 97; i<=122; ++i) {
+            int asciiValue = i;
+            char validCharacter = (char)asciiValue;
+            System.out.println(validCharacter);
+        }
+    }
+
+    public static void printUpperCase()
+    {
+        // TODO: print valid numeric input
+        System.out.println("You've reached the printUpperCase method.");
+        for(int i = 65; i<=90; ++i) {
+            int asciiValue = i;
+            char validCharacter = (char)asciiValue;
+            System.out.println(validCharacter);
+        }
+    }
+}
---
+++ src/controlFlowProject.java
@@ -0,0 +1,9 @@
+public class controlFlowProject {
+    public static void main(String[] args) {
+        // call to do all the things.
+        AsciiChars.printNumbers();
+        AsciiChars.printLowerCase();
+        AsciiChars.printUpperCase();
+    }
+}
+
